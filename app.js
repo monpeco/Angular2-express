d061cfc2a4dd2d4e2f3023e6b96bae45a7d50d2d
monpeco <monpeco@gmail.com>
2017-03-17 01:43:57 +0000

    Initial commit. working app

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+server.listen(process.env.PORT || 9999, function(){
+    console.log("Server connected. Listening on port: 9999");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.use( express.static(__dirname + '/front' ) );
+
+app.get('/test', function(req,res){ //example http request receiver
+  return res.send(myTestVar);
+});
